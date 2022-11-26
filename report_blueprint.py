bee3430302ec0ae347086f3c1e323f2395191d1d
Mzrbx <mauricio.zabalab@gmail.com>
2022-11-25 19:48:35 -0500

    v2

--- report_blueprint.py
+++ report_blueprint.py
@@ -8,28 +8,32 @@ data_config = load_file_config()
 url_base = data_config.get('url-backend-results') + "/report"
 
 
-@report_blueprints.route("/results", methods=['GET'])
+@report_blueprints.route("/report", methods=['GET'])
 def get_highest_votes() -> dict:
     url = url_base + "/highest_votes"
     response = requests.get(url, headers=HEADERS)
     return response.json()
 
 
-@report_blueprints.route("/results/result_by_table", methods=['GET'])
+@report_blueprints.route("/report/result_by_table", methods=['GET'])
 def get_result_by_table() -> dict:
     url = url_base + "/result_by_table"
     response = requests.get(url, headers=HEADERS)
     return response.json()
 
 
-@report_blueprints.route("/result/insert",methods=['GET'])
+@report_blueprints.route("/report/insert",methods=['GET'])
 def get_result_by_candidate() -> dict:
-    result = request.get_json()
     url = url_base + "/result_by_candidate"
     response = requests.get(url, headers=HEADERS)
     return response.json()
 
 
+@report_blueprints.route("/report/result_by_politicalparty",methods=['GET'])
+def get_result_by_candidate() -> dict:
+    url = url_base + "/result_by_politicalparty"
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
 
 
 
